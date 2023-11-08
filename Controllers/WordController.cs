0361c899ef4cea0faf07e5213d150a96588bbffc
Liam J Westhall <liwesthall@gmail.com>
2023-11-08 16:38:14 -0500

    Add project files.

---
+++ Controllers/WordController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Vocas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public WordController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                               select WordId, Term, Def from 
+                               dbo.Words
+                                ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("VocabAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand=new SqlCommand(query,myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Models/Word.cs
@@ -0,0 +1,11 @@
+﻿namespace Vocas.Models
+{
+    public class Word
+    {
+        public int ID { get; set; }
+        public string Term { get; set; }
+        public string Definition { get; set; }
+        public string partOfSpeech { get; set; }
+
+    }
+}
