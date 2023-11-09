beced32379c2587a70b84775bb16310fe48ae11c
Liam J Westhall <liwesthall@gmail.com>
2023-11-09 12:09:00 -0500

    Added language property to word

---
+++ Migrations/20231109163028_AddLanguageToWord.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vocas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLanguageToWord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Words",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Words");
+        }
+    }
+}
--- Models/Word.cs
+++ Models/Word.cs
@@ -7,6 +7,8 @@
         public string PartOfSpeech { get; set; }
         public string Definition { get; set; }
         public string Example { get; set; }
+        
+        public string Language { get; set; }
         // Foreign key
         public int UserId { get; set; }
         // Navigation property
